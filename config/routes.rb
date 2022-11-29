68895f8751110348bd60d4deb5741ac44e46c382
Cicero Sousa <cicerosousacs@hotmail.com>
2022-11-28 21:15:47 -0300

    ajueste de tabelas views

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,4 @@ Rails.application.routes.draw do
   devise_for :admins
 
   root to: 'ativo_pge/welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
